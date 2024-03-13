c36fa24fdb9c5742dc5e5fb440582cc8da779684
evan <00.evanwilson@gmail.com>
2024-03-13 23:29:57 +0000

    done i think

--- rock.py
+++ rock.py
@@ -1,11 +1,10 @@
 import random
 
-#change to sword, gun, shield, hammer
-
 def RPC():
     choices = ['rock', 'paper', 'scissors', 'dirt']
     computer_choice = random.choice(choices)
-    user_choice = input("Enter your choice (rock, paper, scissors, or dirt): ").lower()
+    print("Note that choosing dirt will give a 50% chance of victory.")
+    user_choice = input("Enter your choice of rock, paper, scissors, or dirt: ").lower()
 
     print("Your opponent chose:", computer_choice)
 
@@ -14,7 +13,7 @@ def RPC():
         return
 
     if user_choice == computer_choice:
-        print("Draw!")
+        print("Tie!")
 
     elif user_choice == 'rock':
         if computer_choice == 'scissors':
@@ -25,7 +24,7 @@ def RPC():
             else:
                 print("You lose!")
         else:
-            print("You lose! Bitch")
+            print("You lose!")
 
     elif user_choice == 'paper':
         if computer_choice == 'rock':
@@ -37,7 +36,7 @@ def RPC():
                 print("You lose!")
         else:
             print("You lose!")
-          
+
     elif user_choice == 'scissors':
         if computer_choice == 'paper':
             print("You win!")
@@ -48,11 +47,11 @@ def RPC():
                 print("You lose!")
         else:
             print("You lose!")
-          
+
     elif user_choice == 'dirt':
-        if computer_choice == 'rock' or computer_choice == 'scissors':
-            print("You win!")
+        if random.random() < 0.5:
+            print("You win! The luck of dirt was on your side.")
         else:
-            print("You lose!")
+            print("You lose! The luck of dirt was not on your side.")
 
 RPC()
